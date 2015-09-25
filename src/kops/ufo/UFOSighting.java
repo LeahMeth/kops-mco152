d016e56b6027938e01d202fa2fc5a1a670b9ae73
leahkops <leahkops1@gmail.com>
2015-09-24 20:32:04 -0400

    added for class 9/24

---
+++ src/kops/ufo/UFOSighting.java
@@ -0,0 +1,12 @@
+package kops.ufo;
+
+public class UFOSighting {
+
+	
+	private String sightedAt;
+	private String reportedAt;
+	private String location;
+	private String shape;
+	private String duration;
+	private String desription;
+}
---
+++ src/kops/ufo/UFOSightingMain.java
@@ -0,0 +1,26 @@
+package kops.ufo;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import com.google.gson.Gson;
+
+public class UFOSightingMain {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+
+		BufferedReader in = BufferedReader(new FileReader("ufo_awesome.json"));
+		
+		Gson gson = new Gson();
+		
+		
+		
+		
+		
+		
+		in.close();
+	}
+
+}
