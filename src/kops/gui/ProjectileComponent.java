59043a179cef16ebb7c99ead5f554b9c970eeaed
leahkops <leahkops1@gmail.com>
2015-10-29 00:11:44 -0400

    Added Projectile Arc GUI

---
+++ src/kops/gui/ProjectileComponent.java
@@ -0,0 +1,33 @@
+package kops.gui;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.Line2D;
+
+import javax.swing.JComponent;
+
+public class ProjectileComponent extends JComponent{
+	
+	@Override
+	protected void paintComponent(Graphics g){
+		super.paintComponent(g);
+		
+		double firstX = 0;
+		double firstY = 300;
+		double secondX;
+		double secondY;
+		
+		for(int i = 0; i < 10; i++){
+			Projectile p = new Projectile(i, 31, 20);
+			secondX = p.getX();		
+			secondY =(getHeight() - p.getY()) ;	
+			((Graphics2D) g).draw(new Line2D.Double(firstX, firstY, secondX, secondY));
+			firstX = secondX;
+			firstY = secondY;
+			
+		}
+		
+		
+	}
+
+}
---
+++ src/kops/gui/ProjectileJFrame.java
@@ -0,0 +1,25 @@
+package kops.gui;
+
+import java.awt.BorderLayout;
+import java.awt.Container;
+import javax.swing.JFrame;
+
+public class ProjectileJFrame extends JFrame{
+
+	public static void main(String[] args) {
+		new ProjectileJFrame().setVisible(true);
+	}
+	
+	
+	public ProjectileJFrame(){
+		setTitle("Projectile Arc");
+		setSize(300,300);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		BorderLayout layout = new BorderLayout();
+		Container c = getContentPane();
+		c.setLayout(layout);
+		c.add(new ProjectileComponent(), BorderLayout.CENTER);
+	}
+
+}
