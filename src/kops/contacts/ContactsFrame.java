1b4d9075e1e4c387a5a9ce2a96abbec5a9e3529e
a <a@gmail.com>
2015-12-31 20:07:55 -0500

    added jetty jar and twitter classes

--- src/kops/contacts/ContactsFrame.java
+++ src/kops/contacts/ContactsFrame.java
@@ -1,6 +1,5 @@
 package kops.contacts;
 
-import java.awt.Container;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
@@ -8,30 +7,34 @@ import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JList;
+import javax.swing.JPanel;
 
 public class ContactsFrame extends JFrame{
 
 	private JButton getList;
 	private JList<String> list;
 	private String[] contactNames;
+	private JPanel panel;
 	
 	public ContactsFrame(){
 		setTitle("ContactList");
 		setSize(400,400);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		Container c = getContentPane();
-		setLayout(new BoxLayout(c, BoxLayout.Y_AXIS));
+		panel = new JPanel();
+		panel.setLayout(new BoxLayout(panel,BoxLayout.Y_AXIS ));	
+		
 		
 		getList = new JButton();
 		getList.setText("Get List of Contacts");
 		add(getList);
 		
+		add(panel);
 		
 		list = new JList<String>(contactNames);
 		list.setLayoutOrientation(JList.VERTICAL);
 		
-		add(list);
+		panel.add(list);
 		
 		//when button is clicked, get array of contacts using thread
 		getList.addActionListener(new ActionListener(){
