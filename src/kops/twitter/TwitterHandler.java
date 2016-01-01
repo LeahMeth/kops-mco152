4b329c65d5d898aef9c34e8d2aca4459cdf185df
a <a@gmail.com>
2015-12-31 20:35:03 -0500

    modified twitter handler

--- src/kops/twitter/TwitterHandler.java
+++ src/kops/twitter/TwitterHandler.java
@@ -6,9 +6,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Request;
-import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.AbstractHandler;
 
 public class TwitterHandler extends AbstractHandler {
@@ -19,15 +17,11 @@ public class TwitterHandler extends AbstractHandler {
 			HttpServletRequest request, 
 			HttpServletResponse response) 
 			throws IOException, ServletException{
-		
-		
-	
+
 		response.setContentType("text/html;charset=utf-8");
 		response.setStatus(HttpServletResponse.SC_OK);
 		baseRequest.setHandled(true);
-		response.getWriter().println("<h1>Hello World!</h1>");
-
-		
+		response.getWriter().println("<h1>Hello World!!</h1><h2>Hello World!!</h2>");
 		
 	}
 
